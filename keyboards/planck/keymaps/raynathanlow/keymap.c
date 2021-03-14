27ec24882fd61dbe488aa805223a40c6f56286b2
Ray Nathan Low <raynathanlow@gmail.com>
2021-03-13 23:51:47 -0800

    03-13-2021_1908

--- keyboards/planck/keymaps/raynathanlow/keymap.c
+++ keyboards/planck/keymaps/raynathanlow/keymap.c
@@ -22,58 +22,38 @@ enum planck_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
+  _ONESHOT,
+  _GUI,
+  _MOUSE,
   _ADJUST
 };
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  GUI1,
-  GUI2,
+  LOCK,
   CTLINS,
   SHFTINS,
-  UNDERSCORE,
-  GUIUP,
-  GUIDOWN,
-  GUILEFT,
-  GUIRIGHT
 };
 
 enum tapdancers {
-    T_BR = 0, // [, ]
+    T_1 = 0, // 1, !
+    T_2, // 2, @
+    T_3, // 3, #
+    T_4, // 4, $
+    T_5, // 5, %
+    T_6, // 6, ^
+    T_7, // 7, &
+    T_8, // 8, *
+    T_9, // 9, (
+    T_0, // 0, )
+    T_BR, // [, ]
     T_PA, // (, )
     T_CU, // {, }
     T_GT, // `, ~
     T_ME, // -, =
     T_BP, // \, |
-    T_PS, // +, /
-    T_EXLM_W1, // !, GUI + 1
-    T_AT_W2 // @, GUI + 2
+    T_PU, // +, _
 };
-
-// Maybe I can refactor this so that I don't repeat myself for win2
-void exlm_win1(qk_tap_dance_state_t *state, void *user_data) {
-  switch (state->count) {
-  case 1:
-    SEND_STRING("!");
-    reset_tap_dance(state);
-    break;
-  case 2:
-    register_code16(G(KC_1)); unregister_code16(G(KC_1));
-    break;
-  }
-}
-
-void at_win2(qk_tap_dance_state_t *state, void *user_data) {
-  switch (state->count) {
-  case 1:
-    SEND_STRING("@");
-    reset_tap_dance(state);
-    break;
-  case 2:
-    register_code16(G(KC_2)); unregister_code16(G(KC_2));
-    break;
-  }
-}
 
 void gt(qk_tap_dance_state_t *state, void *user_data) {
   switch (state->count) {
@@ -101,25 +81,32 @@ void bp(qk_tap_dance_state_t *state, void *user_data) {
 
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
+  [T_1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_EXLM),
+  [T_2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_AT),
+  [T_3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_HASH),
+  [T_4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_DLR),
+  [T_5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_PERC),
+  [T_6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_CIRC),
+  [T_7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_AMPR),
+  [T_8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_ASTR),
+  [T_9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_LPRN),
+  [T_0] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_RPRN),
   [T_BR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
   [T_PA] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
   [T_CU] = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
   [T_GT] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_TILD),
   [T_ME] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_EQUAL),
-  [T_BP] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, KC_PIPE), // ACTION_TAP_DANCE_FN b/c need shifted version?
-  [T_PS] = ACTION_TAP_DANCE_DOUBLE(KC_PPLS, KC_PSLS),
-  [T_EXLM_W1] = ACTION_TAP_DANCE_FN (exlm_win1),
-  [T_AT_W2] = ACTION_TAP_DANCE_FN (at_win2)
+  [T_BP] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, KC_PIPE),
+  [T_PU] = ACTION_TAP_DANCE_DOUBLE(KC_PPLS, KC_UNDS),
 };
-
-// Macros and Functions Needed
-// TD_GT
-// TD_BP
 
 // TODO: Update ASCII Representation and align everything
 
-#define LOWER MO(_LOWER)
+#define LOWER TT(_LOWER)
 #define RAISE MO(_RAISE)
+#define ONESHOT OSL(_ONESHOT)
+#define GUI LM(_GUI, MOD_LGUI)
+#define MOUSE TG(_MOUSE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -138,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
-    _______, _______, KC_LGUI, KC_LALT, KC_LCTL, LOWER,   KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    _______, ONESHOT, GUI,     KC_LALT, KC_LCTL, LOWER,   RAISE,   KC_SPC,  MOUSE  , _______, _______, _______
 ),
 
 /* Lower
@@ -153,10 +140,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    TD(T_EXLM_W1),    TD(T_AT_W2), KC_HASH, KC_DLR, KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_7, KC_8, KC_9, KC_BSPC,
-    GUI1,           KC_F8,       KC_F12,   KC_END,   KC_HOME,   TD(T_GT),   TD(T_ME),   TD(T_PA),    KC_4,    KC_5, KC_6, KC_DEL,
-    GUI2,          CTLINS,       SHFTINS,    KC_PGDN,  KC_PGUP,  UNDERSCORE,  TD(T_BR),  TD(T_CU), KC_1, KC_2, KC_3,  TD(T_PS),
-    _______, _______, _______, _______, _______, _______, KC_SPC, TD(T_BP),    KC_0,    KC_COMM, KC_KP_DOT, KC_PENT
+    KC_TAB,  TD(T_1), TD(T_2), TD(T_3), TD(T_4), TD(T_5), TD(T_6), TD(T_7), TD(T_8), TD(T_9),  TD(T_0),  KC_BSPC,
+    KC_ESC,  _______, KC_WBAK, KC_PGDN, KC_PGUP, KC_WFWD, TD(T_GT),TD(T_PA),TD(T_CU),TD(T_BR), TD(T_PU), KC_DEL,
+    KC_LSFT, _______, _______, _______, _______, _______, TD(T_BP),TD(T_ME),KC_COMM, KC_KP_DOT,KC_PENT,  MT(MOD_RSFT, KC_ENT),
+    _______, _______, _______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______,   _______
 ),
 
 /* Raise
@@ -171,10 +158,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    _______,  _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    GUIUP,    _______,    _______,
-    _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______, GUILEFT,  GUIDOWN, GUIRIGHT, _______,
-    _______, _______,   _______,   _______,   KC_CAPS,  _______,  _______,  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,
-    _______, _______, _______, _______, KC_LSFT, _______, _______, _______, _______, _______, _______, _______
+    KC_TAB,  _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,  _______, _______,
+    KC_ESC,  _______, _______, _______, _______, _______, KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,
+    KC_LSFT, _______, _______, _______, _______, _______, KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD,  KC_MPLY, _______,
+    _______, _______, _______, _______, KC_LCTL, _______, _______, _______, _______, _______,  _______, _______
+),
+
+/* ONESHOT
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ONESHOT] = LAYOUT_planck_grid(
+    _______, _______, KC_F2,   KC_F8,   KC_F12,  _______, _______, _______, _______, _______, _______, KC_SLEP,
+    KC_CAPS, _______, _______, KC_END,  KC_HOME, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, CTLINS,  SHFTINS, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LOCK
+),
+
+/* GUI
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_GUI] = LAYOUT_planck_grid(
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, KC_UP,   _______, _______, _______,
+    KC_ESC,  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+    KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+/* MOUSE
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_MOUSE] = LAYOUT_planck_grid(
+    _______, _______, _______, KC_MS_U, _______, _______, _______, KC_WH_D, KC_WH_U, _______, _______, _______,
+    _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, KC_WH_L, KC_BTN1, KC_BTN2, KC_WH_R, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -191,7 +232,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______,  _______,  _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______,  _______,_______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
@@ -211,14 +252,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
         break;
-    case GUI1:
+    case LOCK:
         if (record -> event.pressed) {
-            SEND_STRING(SS_LGUI("1"));
-        }
-        break;
-    case GUI2:
-        if (record -> event.pressed) {
-            SEND_STRING(SS_LGUI("2"));
+            SEND_STRING(SS_LGUI("l"));
         }
         break;
     case CTLINS:
@@ -229,31 +265,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case SHFTINS:
         if (record -> event.pressed) {
             SEND_STRING(SS_LSFT(SS_TAP(X_INS)));
-        }
-        break;
-    case UNDERSCORE:
-        if (record -> event.pressed) {
-            SEND_STRING("_");
-        }
-        break;
-    case GUIUP:
-        if (record -> event.pressed) {
-            SEND_STRING(SS_LGUI(SS_TAP(X_UP)));
-        }
-        break;
-    case GUIDOWN:
-        if (record -> event.pressed) {
-            SEND_STRING(SS_LGUI(SS_TAP(X_DOWN)));
-        }
-        break;
-    case GUILEFT:
-        if (record -> event.pressed) {
-            SEND_STRING(SS_LGUI(SS_TAP(X_LEFT)));
-        }
-        break;
-    case GUIRIGHT:
-        if (record -> event.pressed) {
-            SEND_STRING(SS_LGUI(SS_TAP(X_RIGHT)));
         }
         break;
     }
