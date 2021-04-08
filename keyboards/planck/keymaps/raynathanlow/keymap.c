aed4d4a3015a8f06b08cf117dc41348f34641042
Ray Nathan Low <raynathanlow@gmail.com>
2021-04-07 21:12:27 -0700

    04-03-2021_1417.1
    Add a way to go back to default QWERTY layer from gaming layer

--- keyboards/planck/keymaps/raynathanlow/keymap.c
+++ keyboards/planck/keymaps/raynathanlow/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        KC_Q,    KC_W,      KC_E,  KC_R,    KC_T,    KC_Y,       KC_U,   KC_I,    KC_O,    KC_P,     KC_BSPC,
     KC_ESC,        GUI_A,   ALT_S,     SFT_D, CTL_F,   KC_G,    KC_H,       CTL_J,  SFT_K,   ALT_L,   GUI_SCLN, KC_QUOT,
     OSM(MOD_LSFT), KC_Z,    KC_X,      KC_C,  KC_V,    KC_B,    KC_N,       KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
-    OSL(LAYERS),    _______, _______,   ALT,   SYMBOLS, ONESHOT, NAVIGATION, KC_SPC, KC_ESC,  KC_DEL,  KC_DOWN,  KC_UP
+    LAYERS,    _______, _______,   ALT,   SYMBOLS, ONESHOT, NAVIGATION, KC_SPC, KC_ESC,  KC_DEL,  KC_DOWN,  KC_UP
 ),
 
 /* COLEMAK
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        KC_Q,    KC_W,    KC_F,   KC_P,    KC_G,    KC_J,       KC_L,   KC_U,    KC_Y,   KC_SCLN, KC_BSPC,
     KC_ESC,        HOME_A,  HOME_R,  HOME_S, HOME_T,  KC_D,    KC_H,       HOME_N, HOME_E,  HOME_I, HOME_O,  KC_QUOT,
     OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_K,       KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-    OSL(LAYERS),    _______, _______, ALT,    SYMBOLS, ONESHOT, NAVIGATION, KC_SPC, KC_ESC,  KC_DEL, KC_DOWN, KC_UP
+    OSL(_LAYERS),    _______, _______, ALT,    SYMBOLS, ONESHOT, NAVIGATION, KC_SPC, KC_ESC,  KC_DEL, KC_DOWN, KC_UP
 ),
 
 /* One Shot
@@ -236,14 +236,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sh/En |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |j2
  * `-----------------------------------------------------------------------------------'
  */
 [_GAMING] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_Y,    KC_U,   KC_I,    KC_O,              KC_P,            KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_H,    KC_J,   KC_K,    KC_L,              KC_SCLN,         KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_N,    KC_M,   KC_COMM, KC_DOT,            KC_SLSH,         KC_ENT,
-    KC_LCTL, KC_4,    KC_3,    KC_2,    KC_SPC,  KC_1, KC_LBRC, KC_RBRC,_______, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, OSL(LAYERS)
+    KC_LCTL, KC_4,    KC_3,    KC_2,    KC_SPC,  KC_1, KC_LBRC, KC_RBRC,_______, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, TO(_QWERTY)
 ),
 
 /* Adjust (Lower + Raise)
