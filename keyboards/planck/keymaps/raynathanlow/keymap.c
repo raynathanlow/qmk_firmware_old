361ded09fb6979cdd9d0b73f8d6a62e58cec4a9f
Ray Nathan Low <raynathanlow@gmail.com>
2021-05-24 15:51:46 -0700

    Add layer with function keys only

--- keyboards/planck/keymaps/raynathanlow/keymap.c
+++ keyboards/planck/keymaps/raynathanlow/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-enum planck_layers { _QWERTY, _COLEMAK, _ONESHOT, _LAYERS, _SYMBOLS, _NAVIGATION, _MOUSE, _GAMING, _MEDIA, _ADJUST, _ALT };
+enum planck_layers { _QWERTY, _COLEMAK, _ONESHOT, _LAYERS, _SYMBOLS, _NAVIGATION, _MOUSE, _GAMING, _MEDIA, _FUNCTION, _ADJUST, _ALT };
 
 enum planck_keycodes {
     QWERTY = SAFE_RANGE,
@@ -56,6 +56,7 @@ uint16_t alt_tab_timer     = 0;
 #define GAMING TO(_GAMING)
 #define MEDIA TO(_MEDIA)
 #define ALT LM(_ALT, MOD_LALT)
+#define FUNCTION MO(_FUNCTION)
 
 // Left-hand home row mods
 #define GUI_A LGUI_T(KC_A)
@@ -81,7 +82,6 @@ uint16_t alt_tab_timer     = 0;
 #define HOME_I LALT_T(KC_I)
 #define HOME_O RGUI_T(KC_O)
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Default
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        KC_Q,    KC_W,      KC_E,  KC_R,    KC_T,    KC_Y,       KC_U,   KC_I,    KC_O,    KC_P,     KC_BSPC,
     KC_ESC,        GUI_A,   ALT_S,     SFT_D, CTL_F,   KC_G,    KC_H,       CTL_J,  SFT_K,   ALT_L,   GUI_SCLN, KC_QUOT,
     OSM(MOD_LSFT), KC_Z,    KC_X,      KC_C,  KC_V,    KC_B,    KC_N,       KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
-    LAYERS,    _______, _______,   ALT,   SYMBOLS, ONESHOT, NAVIGATION, KC_SPC, KC_ESC,  KC_DEL,  KC_DOWN,  KC_UP
+    LAYERS,    FUNCTION, _______,   ALT,   SYMBOLS, ONESHOT, NAVIGATION, KC_SPC, KC_ESC,  KC_DEL,  KC_DOWN,  KC_UP
 ),
 
 /* COLEMAK
@@ -244,6 +244,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_H,    KC_J,   KC_K,    KC_L,              KC_SCLN,         KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_N,    KC_M,   KC_COMM, KC_DOT,            KC_SLSH,         KC_ENT,
     KC_LCTL, KC_4,    KC_3,    KC_2,    KC_SPC,  KC_1, KC_LBRC, KC_RBRC,KC_PSCR, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, TO(_QWERTY)
+),
+
+/* Gaming
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sh/En |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |j2
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FUNCTION] = LAYOUT_planck_grid(
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
