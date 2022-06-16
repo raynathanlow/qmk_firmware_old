76821d4cbf051bb125834eba6c98277c13401d97
Ray Nathan Low <raynathanlow@gmail.com>
2022-06-16 11:26:42 -0700

    Change to GACS, combine sym and num, add left nav
    Also added swapper, media, and mouse keys

--- keyboards/planck/keymaps/raynathanlow/keymap.c
+++ keyboards/planck/keymaps/raynathanlow/keymap.c
@@ -17,131 +17,180 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-enum planck_layers { _QWERTY, _COLEMAK, _SYMBOLS, _NUMBERS, _NAVIGATION, _FUNCTION, _ADJUST, _ALT };
+enum planck_layers {
+    _QWERTY,
+    _COLEMAK,
+    _SYMBOLS,
+    _NUMBERS,
+    _NAVIGATION,
+    _LEFT_NAVIGATION,
+    _MEDIA,
+    _GAME,
+    _SWAPPER,
+    _FUNCTION,
+    _ADJUST,
+};
 
 enum planck_keycodes {
     QWERTY = SAFE_RANGE,
     COLEMAK,
-    A_TAB,     // alt tab
-    A_F4,      // close window
-    C_INS,     // copy in some terminals
-    S_INS,     // paste in some terminals
-    G_L,       // lock
-    SNIP,      // screenshot
-    G_TAB,     // task view
-    GC_LEFT,   // left virtual desktop
-    GC_RIGHT,  // right virtual desktop
-    G_1,       // 1st pinned program in Taskbar
-    G_2,       // 2st pinned program in Taskbar
-    G_E,       // Windows Explorer,
-    G_LEFT,    // maximize window to left side of screen
-    G_DOWN,    // minimize window
-    G_UP,      // maximize window
-    G_RIGHT,   // maximize window to right side of screen
-    GS_LEFT,   // move window to another monitor
-    GS_RIGHT   // move window to another monitor
+    A_F4,
+    SNIP,      // Open Snip & Sketch
+    ALT_TAB
 };
 
-// Super Alt Tab
-// Needs to be near the beginning
-// https://beta.docs.qmk.fm/using-qmk/advanced-keycodes/feature_macros#super-alt-tab
-bool     is_alt_tab_active = false;
-uint16_t alt_tab_timer     = 0;
+bool is_alt_tab_active = false;
 
-#define SYM MO(_SYMBOLS)
-#define NUM MO(_NUMBERS)
-#define NAV LT(_NAVIGATION, KC_ESC)
-#define ALT LM(_ALT, MOD_LALT)
-#define FUN MO(_FUNCTION)
+#define SYM LT(_SYMBOLS, KC_SPC)
+#define NUM TG(_NUMBERS)
+#define NAV LT(_NAVIGATION, KC_SPC)
+#define L_NAV MO(_LEFT_NAVIGATION)
+#define SWAPPER MO(_SWAPPER)
+#define MEDIA MO(_MEDIA)
+#define FUNCTION OSL(_FUNCTION)
+#define GAME TG(_GAME)
 
-// Left-hand home row mods
-#define GUI_A LGUI_T(KC_A)
-#define ALT_S LALT_T(KC_S)
-#define SFT_D LSFT_T(KC_D)
-#define CTL_F LCTL_T(KC_F)
-
-// Right-hand home row mods
-#define CTL_J RCTL_T(KC_J)
-#define SFT_K RSFT_T(KC_K)
-#define ALT_L LALT_T(KC_L)
-#define GUI_SCLN RGUI_T(KC_SCLN)
-
+// QWERTY
 // Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
-#define HOME_R LALT_T(KC_R)
-#define HOME_S LSFT_T(KC_S)
-#define HOME_T LCTL_T(KC_T)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_N RCTL_T(KC_N)
-#define HOME_E RSFT_T(KC_E)
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+
+// Colemak
+// Left-hand home row mods
+#define HOME_R LALT_T(KC_R)
+#define COLE_S LCTL_T(KC_S)
+#define HOME_T LSFT_T(KC_T)
+
+// Right-hand home row mods
+#define HOME_N RSFT_T(KC_N)
+#define HOME_E RCTL_T(KC_E)
 #define HOME_I LALT_T(KC_I)
 #define HOME_O RGUI_T(KC_O)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R,    KC_T, KC_Y, KC_U,   KC_I,    KC_O,    KC_P,      KC_BSPC,
-    NAV,     KC_A,    KC_S,    KC_D, KC_F,    KC_G, KC_H, KC_J,   KC_K,    KC_L,    KC_SCOLON, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V,    KC_B, KC_N, KC_M,   KC_COMM, KC_DOT,  KC_SLSH,   RSFT_T(KC_ENT),
-    KC_ESC, _______,  KC_LGUI, ALT,  KC_LCTL, SYM,  NUM,  KC_SPC, FUN,     _______, SNIP,      KC_PSCR
+    KC_TAB,         KC_Q,          KC_W,    KC_E,   KC_R,   KC_T,     KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,     KC_BSPC,
+    RCTL_T(KC_ESC), KC_A,          HOME_S,  HOME_D, HOME_F, KC_G,     KC_H,   HOME_J, HOME_K,   HOME_L,  KC_SCLN,  KC_QUOT,
+    KC_LSFT,        KC_Z,          KC_X,    KC_C,   KC_V,   KC_B,     KC_N,   KC_M,   KC_COMMA, KC_DOT,  KC_SLASH, RSFT_T(KC_ENT),
+    KC_ESC,         OSM(MOD_LGUI), ALT_TAB, L_NAV,  SYM,    FUNCTION, MEDIA,  NAV,    KC_DEL,   _______, _______,  GAME
 ),
 
 [_COLEMAK] = LAYOUT_planck_grid(
     _______, _______, _______, KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCOLON, _______,
-    _______, _______, KC_R,    KC_S,    KC_T,    KC_D,    _______, KC_N,    KC_E,    KC_I,    KC_O,      _______,
+    _______, KC_A,    HOME_R,  COLE_S,  HOME_T,  KC_D,    _______, HOME_N,  HOME_E,  HOME_I,  KC_O,      _______,
     _______, _______, _______, _______, _______, _______, KC_K,    _______, _______, _______, _______,   _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______
 ),
 
 [_SYMBOLS] = LAYOUT_planck_grid(
-    A_F4,    KC_EXCLAIM, KC_AT,   KC_HASH,   KC_DOLLAR,     KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN,       KC_RIGHT_PAREN,       _______,
-    KC_CAPS, KC_GRAVE,   KC_PIPE, KC_BSLASH, KC_UNDERSCORE, _______,    _______,       KC_MINUS,     KC_EQUAL,    KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_DEL,
-    _______, _______,    KC_WBAK, KC_PGUP,   KC_PGDN,       KC_WFWD,    _______,       KC_TILDE,     KC_PLUS,     KC_LBRACKET,         KC_RBRACKET,          _______,
-    _______, _______,    _______, _______,   _______,       _______,    _______,       _______,      _______,     _______,             _______,              _______
+    A_F4,          KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,          KC_7,         KC_8,        KC_9,          KC_0,           KC_DEL,
+    KC_CAPS,       KC_EXCLAIM, KC_AT,      KC_HASH,    KC_DOLLAR,  KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_UNDS,
+    OSM(MOD_LGUI), LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_S), KC_PLUS,       KC_MINS,      KC_EQL,      KC_LCBR,       KC_RCBR,        KC_PIPE,
+    _______,       _______,    _______,    _______,    _______,    _______,    KC_GRAVE,      _______,      KC_BSLS,     KC_LBRC,       KC_RBRC,        KC_TILD
 ),
 
 [_NUMBERS] = LAYOUT_planck_grid(
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,        KC_9,    KC_0,    KC_BSPC,
-    _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, KC_PLUS,  KC_COMM,     KC_DOT,  KC_SLSH, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_MINUS, KC_ASTERISK, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,  _______,     _______, _______, _______
+    _______, _______, _______, _______, _______, _______, KC_NLCK, KC_P7,  KC_P8,   KC_P9,   KC_PMNS, KC_BSPC,
+    _______, _______, _______, _______, _______, _______, _______, KC_P4,  KC_P5,   KC_P6,   KC_PPLS, KC_PAST,
+    _______, _______, _______, _______, _______, _______, KC_COMM, KC_P1,  KC_P2,   KC_P3,   KC_EQL,  _______,
+    _______, _______, _______, _______, KC_SPC,  _______, KC_SPC,  KC_P0,  KC_COMM, KC_PDOT, KC_PSLS, _______
 ),
 
 [_NAVIGATION] = LAYOUT_planck_grid(
-    _______, _______, _______, _______,  _______, _______, _______, _______, KC_UP,   _______,   _______, _______,
-    _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,  _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT,  _______, _______,
-    _______, _______, _______, KC_HOME, KC_END,   _______, _______, KC_INS,  KC_HOME, KC_PGUP,   _______, _______,
-    _______, _______, _______, _______, _______,  _______, _______, KC_DEL,  KC_END,  KC_PGDOWN, _______, _______
+    _______, _______, _______, KC_LBRC, KC_RBRC, _______, KC_PAUS, KC_APP,  KC_UP,   KC_INS,   KC_SCROLLLOCK, _______,
+    _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,        _______,
+    _______, _______, _______, _______, KC_TILD, _______, _______, _______, _______, _______,  _______,       _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,       _______
+),
+
+[_LEFT_NAVIGATION] = LAYOUT_planck_grid(
+    KC_MYCM, KC_PGUP, KC_HOME, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______,
+    KC_WBAK, KC_PGDN, KC_END,  KC_WFWD, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
+    _______, _______, _______, _______, NUM,     _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_MEDIA] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLU, KC_MPLY, KC_MNXT, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_MRWD, KC_VOLD, KC_MFFD, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_GAME] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,    KC_W,   KC_E, KC_R,     KC_T, _______, _______, _______, _______, _______, _______,
+    KC_LCTL, KC_A,    KC_S,   KC_D, KC_F,     KC_G, KC_H, _______, _______, _______, _______, _______,
+    KC_LSFT, KC_Z,    KC_X,   KC_C, KC_V,     KC_B, _______, _______, _______, _______, _______, _______,
+    KC_LCTL, _______, KC_ESC, KC_4, KC_SPACE, _______, _______, _______, _______, _______, KC_PSCR, GAME
+),
+
+[_SWAPPER] = LAYOUT_planck_grid(
+    KC_ESC,  KC_UP,   KC_DEL,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_FUNCTION] = LAYOUT_planck_grid(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, KC_F12,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    _______, KC_LGUI, KC_LALT,  KC_LCTL, KC_LSFT, _______, _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
+    _______, _______, _______,  KC_PSCR, SNIP,    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_ADJUST] = LAYOUT_planck_grid(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, QWERTY,  COLEMAK,  _______,  _______,_______,
+    QWERTY,  _______, MU_MOD,  AU_ON,   AU_OFF,  _______, KC_TAPP, KC_TAPD, KC_TAPU,  _______, _______, COLEMAK,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
-),
-
-[_ALT] = LAYOUT_planck_grid(
-    _______, _______, KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, KC_DEL,  _______, _______, _______, KC_DEL,  KC_RSFT, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SYMBOLS, _NUMBERS, _ADJUST);
+  return update_tri_layer_state(state, _FUNCTION, _MEDIA, _ADJUST);
+}
+
+bool is_swapper_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+        case KC_UP:
+        case KC_LEFT:
+        case KC_DOWN:
+        case KC_RGHT:
+        case KC_DEL:
+            return true;
+        default:
+            return false;
+    }
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == ALT_TAB) {
+        if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+                is_alt_tab_active = true;
+                register_code(KC_LALT);
+                layer_on(_SWAPPER);
+            }
+            register_code(KC_TAB);
+            } else {
+            unregister_code(KC_TAB);
+            }
+    } else if (is_alt_tab_active && !is_swapper_ignored_key(keycode)) {
+        unregister_code(KC_LALT);
+        is_alt_tab_active = false;
+        layer_off(_SWAPPER);
+        return false;
+    }
+
     switch (keycode) {
         case QWERTY:
             if (record->event.pressed) {
@@ -156,18 +205,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
-        case A_TAB:
-            if (record->event.pressed) {
-                if (!is_alt_tab_active) {
-                    is_alt_tab_active = true;
-                    register_code(KC_LALT);
-                }
-                alt_tab_timer = timer_read();
-                register_code(KC_TAB);
-            } else {
-                unregister_code(KC_TAB);
-            }
-            break;
         case A_F4:
             if (record->event.pressed) {
                 register_code(KC_LALT);
@@ -175,150 +212,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_LALT);
             }
             break;
-        case C_INS:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTL(SS_TAP(X_INS)));
-            }
-            break;
-        case S_INS:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LSFT(SS_TAP(X_INS)));
-            }
-            break;
-        case G_L:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI("l"));
-            }
-            break;
         case SNIP:
             if (record->event.pressed) {
                 SEND_STRING(SS_LGUI(SS_LSFT("s")));
-            }
-            break;
-        case G_TAB:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_TAP(X_TAB)));
-            }
-            break;
-        case GC_LEFT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_LCTRL(SS_TAP(X_LEFT))));
-            }
-            break;
-        case GC_RIGHT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_LCTRL(SS_TAP(X_RIGHT))));
-            }
-            break;
-        case G_1:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI("1"));
-            }
-            break;
-        case G_2:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI("2"));
-            }
-            break;
-        case G_E:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI("e"));
-            }
-            break;
-        case G_LEFT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_TAP(X_LEFT)));
-            }
-            break;
-        case G_DOWN:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_TAP(X_DOWN)));
-            }
-            break;
-        case G_UP:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_TAP(X_UP)));
-            }
-            break;
-        case G_RIGHT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_TAP(X_RIGHT)));
-            }
-            break;
-        case GS_LEFT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_LEFT))));
-            }
-            break;
-        case GS_RIGHT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_RIGHT))));
             }
             break;
     }
     return true;
 }
 
-void matrix_scan_user(void) {  // The very important timer.
-    if (is_alt_tab_active) {
-        if (timer_elapsed(alt_tab_timer) > 1000) {
-            unregister_code(KC_LALT);
-            is_alt_tab_active = false;
-        }
-    }
-}
-
-// TAPPING_FORCE_HOLD_PER_KEY
-bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case GUI_A:
-        case ALT_S:
-        case SFT_D:
-        case CTL_F:
-        case CTL_J:
-        case SFT_K:
-        case ALT_L:
-        case GUI_SCLN:
-            return true;
-        default:
-            return false;
-    }
-}
-
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case GUI_A:
-        case GUI_SCLN:
+        // GUI
+        case HOME_A:
+        case HOME_SCLN:
+        case HOME_O:
             return TAPPING_TERM;
-        case ALT_S:
-        case ALT_L:
+        // ALT
+        case HOME_S:
+        case HOME_L:
+        case HOME_R:
+        case HOME_I:
             return TAPPING_TERM;
-        case SFT_D:
-        case SFT_K:
+        // SHIFT
+        case HOME_D:
+        case HOME_K:
+        case COLE_S:
+        case HOME_E:
             return TAPPING_TERM;
-        case CTL_F:
-        case CTL_J:
+        // CONTROL
+        case HOME_F:
+        case HOME_J:
+        case HOME_T:
+        case HOME_N:
             return TAPPING_TERM;
         default:
             return TAPPING_TERM;
     }
 }
-
-// bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-//     switch (keycode) {
-//         case GUI_A:
-//         case GUI_SCLN:
-//             return false;
-//         case ALT_S:
-//         case ALT_L:
-//             return false;
-//         case SFT_D:
-//         case SFT_K:
-//             return true;
-//         case CTL_F:
-//         case CTL_J:
-//             return true;
-//         default:
-//             return false;
-//     }
-// }
